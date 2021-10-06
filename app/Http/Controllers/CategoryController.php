8f202533ae6e960f8a908893798f146b5e8f1350
MoEmam203 <mostafa.emamdar4@gmail.com>
2021-10-06 11:21:14 +0200

    using CategoryRequest for Validation

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -9,7 +10,6 @@ class CategoryController extends Controller
 {
     public function index(){
         $categories = Category::all();
-
         return view("categories.index")->with("categories",$categories);
     }
 
@@ -22,13 +22,16 @@ class CategoryController extends Controller
         return view("categories.create");
     }
 
-    public function store(Request $request){
-        $validated = $request->validate([
-            'name' => 'required|min:3|unique:categories,name'
-        ]);
+    // public function store(Request $request){
+    
+        // $validated = $request->validate([
+        //     'name' => 'required|min:3|unique:categories,name'
+        // ]);
 
-        Category::create($validated);
+        // Category::create($validated);
 
+    public function store(CategoryRequest $request){
+        Category::create($request->validated());
         return redirect()->back()->with("success","Category stored successfully");
     }
 
@@ -36,19 +39,21 @@ class CategoryController extends Controller
         return view("categories.edit")->with("category",$category);
     }
 
-    public function update(Request $request,Category $category){
-        $validated = $request->validate([
-            'name' => 'required|min:3|unique:categories,name,'.$category->id
-        ]);
-
-        $category->update($validated);
-
+    // public function update(Request $request,Category $category){
+        
+        // $validated = $request->validate([
+        //     'name' => 'required|min:3|unique:categories,name,'.$category->id
+        // ]);
+        
+        // $category->update($validated);
+    
+    public function update(CategoryRequest $request,Category $category){
+        $category->update($request->validated());
         return redirect()->route('categories.index')->with("success","Category updated successfully");
     }
 
     public function destroy(Category $category){
         $category->delete();
-
         return redirect()->route('categories.index')->with("success","Category deleted successfully");
     }
 }
