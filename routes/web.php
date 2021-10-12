973391e2899b5c7745fe3da3e3a61170aec05bba
MoEmam203 <mostafa.emamdar4@gmail.com>
2021-10-12 16:14:30 +0200

    change in routes.php

--- routes/web.php
+++ routes/web.php
@@ -28,31 +28,9 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
-// Articles CRUD
-Route::group(['prefix'=>'articles'],function(){
-
-    Route::get("",[ArticleController::class,'index'])->name("articles.index");
-    Route::get("/create",[ArticleController::class,'create'])->middleware("auth")->name("articles.create");
-    Route::post("",[ArticleController::class,'store'])->middleware("auth")->name("articles.store");
-    Route::get("/{article}",[ArticleController::class,'show'])->name("articles.show");
-    Route::get("{article}/edit",[ArticleController::class,'edit'])->name("articles.edit");
-    Route::put("/{article}",[ArticleController::class,'update'])->name("articles.update");
-    Route::delete("/{article}",[ArticleController::class,'destroy'])->name("articles.destroy");
-});
-
-
-// Categories CRUD
-// Route::group(['prefix'=>'categories','middleware'=>'auth'],function(){
-    // Route::get('',[CategoryController::class,'index'])->name('categories.index');
-    // Route::view('/create', 'categories.create')->name('categories.create');
-    // Route::get('/{category}',[CategoryController::class,'show'])->name('categories.show');
-    // Route::post('',[CategoryController::class,'store'])->name('categories.store');
-    // Route::get('/{category}/edit',[CategoryController::class,'edit'])->name('categories.edit');
-    // Route::put('/{category}',[CategoryController::class,'update'])->name("categories.update");
-    // Route::delete('/{category}',[CategoryController::class,'destroy'])->name('categories.destroy');
-// });
-
+// Admin 
 Route::group(['middleware'=>'auth'],function(){
     Route::resource('categories', CategoryController::class);
     Route::resource('tags', TagController::class);
+    Route::resource('articles', ArticleController::class);
 });
